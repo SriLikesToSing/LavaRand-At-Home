31dcb77861c7544b7edafa502106e32170d9e5b7
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2023-09-20 19:10:31 -0700

    proper works

--- cryptoLamp/randomGenerator.py
+++ cryptoLamp/randomGenerator.py
@@ -6,6 +6,7 @@ from hashlib import sha1
 #https://www.computerworld.com/article/3173616/the-sha1-hash-function-is-now-completely-unsafe.html
 
 filename = "seed_val.txt"
+lavaImage = ""
 
 
 #shoudl we incrypt the n file?
@@ -21,10 +22,11 @@ def load_seed(filename=filename):
         # change datatype accordingly (numpy.random.random() returns a float)
         return int(f.read())
 
-def generateNumber(Low, High, Size, n):
+def generateNumber(Low, High, Size):
     #hash the image produced by physically random lavalamp
-    hashedImage = sha1(open("opencv_frame_2.png", 'rb').read()).hexdigest()
-    rng = np.random.default_rng(int(hashedImage, 16)+n)
+    hashedImage = sha1(open(lavaImage, 'rb').read()).hexdigest()
+    seed = np.random.default_rng(int(hashedImage, 16))
+    rng = np.random.default_rng(int(hashedImage, 16)+seed.integers(low=1, high=500000, size=1))
     rint = rng.integers(low=Low, high=High, size=Size)
 
     return list(rint)
@@ -32,15 +34,36 @@ def generateNumber(Low, High, Size, n):
 def boink():
     save_seed(load_seed()+1)
 
+print("type 'done' when finished")
+lavaImage = input("input image name from lavalamp \n")
 
-#print(generateNumber(0, 10, 10, load_seed()))
+if(lavaImage.endswith('.png') or lavaImage.endswith('.jpg')):
+    pass
+elif lavaImage == 'done':
+    exit(0)
+else:
+    raise Exception("Input valid image")
+
 #print(load_seed())
 
+print("type low, high, and number of values to be generated separated by comma. ex: 2,50,5")
+print("if you want to input a new lava lamp image type 'new image' \n")
+print("type 'done' when finished")
+INPUT = ""
 
-hashedImage = sha1(open("opencv_frame_2.png", 'rb').read()).hexdigest()
-rng = np.random.default_rng(int(hashedImage, 16)+load_seed())
-rint = rng.integers(low=1, high=2, size=1)
-print(rint)
+while INPUT != 'done':
+    INPUT = input()
+    if INPUT == 'done':
+        break
+    if INPUT == 'new image':
+        lavaImage = input()
+        if(lavaImage.endswith('.png') or lavaImage.endswith('.jpg')):
+            continue
+        else:
+            raise Exception("Input valid image")
 
-print(load_seed())
+    array = INPUT.split(',')
+    print(generateNumber(int(array[0]), int(array[1]), int(array[2])))
+
+
 
